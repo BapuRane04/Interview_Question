69fada73cd89e34ff8beed61eaee39a38fd0559a
Bapu Rane <bapurane41096@gmail.com>
2026-01-09 20:20:27 +0530

    Factorial

---
+++ Factorial.java
@@ -0,0 +1,11 @@
+package Interview_Question;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int n =5 , fact =1;
+        for(int i=1;i<=n;i++){
+            fact = fact *i;
+        }
+        System.out.println("Factorial of " + n + " is: " + fact);
+    }
+}
